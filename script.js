d6185e14474eb2bbc4e11a2cce9f902793391aa4
Diana Saker <Diana Saker>
2016-08-21 13:04:50 +0300

    func4

---
+++ script.js
@@ -0,0 +1,31 @@
+
+function israel() {
+           document.getElementById("Israel").className="name1";
+        }
+function israel2 () {
+           document.getElementById("Israel").className="p";
+        }
+function USA() {
+           document.getElementById("USA").className="name1";
+        }
+function USA2 () {
+           document.getElementById("USA").className="p";
+        }
+function Italy() {
+           document.getElementById("Italy").className="name1";
+        }
+function Italy2 () {
+           document.getElementById("Italy").className="p";
+        }
+function Holland() {
+           document.getElementById("Holland").className="name1";
+        }
+function Holland2 () {
+           document.getElementById("Holland").className="p";
+        }
+ function France() {
+           document.getElementById("France").className="name1";
+        }
+function France2 () {
+           document.getElementById("France").className="p";
+        }
